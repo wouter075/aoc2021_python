0f661f4bd72ba658031fd14c201eadba25a181b6
Wouter <wouter075@gmail.com>
2021-12-02 11:49:40 +0100

    Small change

--- day2b.py
+++ day2b.py
@@ -1,19 +1,15 @@
 with open('day2.txt') as f:
     lines = [line.rstrip() for line in f]
 
-# reset all values:
-hor = 0
-depth = 0
-aim = 0
+hor, depth, aim = 0, 0, 0
 for x in range(len(lines)):
     d, m = lines[x].split(" ")
     m = int(m)
     if d == "forward":
-        hor += m
+        hor += int(m)
         depth += m * aim
     if d == "down":
-        aim += m
+        aim += int(m)
     if d == "up":
-        aim -= m
-
+        aim -= int(m)
 print(f"Part 2: {int(hor) * int(depth)}")

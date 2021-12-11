f36c56c2bec11a650a61700152d3f47569460725
Wouter <wouter075@gmail.com>
2021-12-11 14:36:09 +0100

    Almost there day8 - part 2

--- day8a.py
+++ day8a.py
@@ -5,7 +5,7 @@ segments = []
 signals = []
 known_len = {2, 3, 4, 7}
 part1 = 0
-
+part2 = 0
 
 for line in lines:
     parts = line.split(" | ")
@@ -18,20 +18,78 @@ for signal in signals:
             part1 += 1
 
 # part 2
-for segment in segments:
+for i in range(len(segments)):
+    segment = segments[i]
+    signal = signals[i]
     unknown = []
+    words = {}
+    known = {}
+
+    for i in range(10):
+        known[i] = {}
+        words[i] = {}
+
     for p in segment.split(" "):
-        if len(p) not in known_len:
+        if len(p) == 2:
+            known[1] = "".join(sorted(p))
+        if len(p) == 3:
+            known[7] = "".join(sorted(p))
+        if len(p) == 4:
+            known[4] = "".join(sorted(p))
+        if len(p) == 7:
+            known[8] = "".join(sorted(p))
+
+        else:
             unknown.append("".join(sorted(p)))
-            # print("".join(sorted(p)))
+
+    u2 = unknown
+    for u in range(10):
+        if len(known[u]) > 0:
+            try:
+                u2.remove(known[u])
+            except ValueError:
+                pass
+
+    while len(u2) is not 0:
+        for p in unknown:
             if len(p) == 5:
-                # 2, 3, 5<
-                # if there are 3 the same parts in the other string, its 5
-                # https://www.w3schools.com/python/trypython.asp?filename=demo_ref_set_intersection
-                sort = "".join(sorted(p))
-                print(sort)
-    print()
+                # 2
+                if len(set(p).intersection(known[4])) == 2:
+                    known[2] = "".join(sorted(p))
+                    u2.remove(p)
+                # 3
+                if len(set(p).intersection(known[1])) == 2:
+                    known[3] = "".join(sorted(p))
+                    u2.remove(p)
+                # 5
+                if len(known[3]) > 0:
+                    if len(set(p).intersection(known[3])) == 4:
+                        known[5] = "".join(sorted(p))
+                        try:
+                            u2.remove(p)
+                        except ValueError:
+                            continue
+            if len(p) == 6:
+                # 6
+                if len(set(p).intersection(known[1])) == 1:
+                    known[6] = "".join(sorted(p))
+                    u2.remove(p)
+                # 9
+                if len(set(p).intersection(known[4])) == 4:
+                    known[9] = "".join(sorted(p))
+                    u2.remove(p)
 
-
+        if len(u2) == 1:
+            known[0] = u2[0]
+            u2.remove(u2[0])
+    print(known)
+    # we know all the segments!
+    sum = ""
+    print(signal)
+    for s in signal.split(" "):
+        s = "".join(sorted(s))
+        sum += str(list(known.keys())[list(known.values()).index(s)])
+    part2 += int(sum)
 
 print(f'Part 1: {part1}')
+print(f'Part 2: {part2}')

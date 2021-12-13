53a9db7744037ed8ef489291ce190a5fca5955d5
Wouter <wouter075@gmail.com>
2021-12-13 23:09:19 +0100

    Changed 11

--- day11a.py
+++ day11a.py
@@ -15,14 +15,18 @@ for _ in range(3):
     flash_list = []
     for l in range(len(grid)):
         line = grid[l]
-        for c in line:
+        for col in range(len(line)):
+            c = grid[l][col]
             if c > 9:
-                flash_list.append({l, c})
+                flash_list.append([l, col])
 
-    while len(flash_list):
-        flashed += 1
+    # while len(flash_list):
+    flashed += 1
+    row, col = flash_list[0]
 
+    # get surrounding:
+    for i in range(col - 1,  col + 2):
+        for y in range(row - 1, row + 2):
+            print(f'{i},{y}: {grid[i][y]}')
 
-
-    print(flash_list)
 
